2e496fa072c5d8884fd9c02a688f25a3a8f2882d
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-10-29 00:55:59 +0200

    gen-cordova-project.sh: Comment out removal of cordova-tizen, add command that adds tizen platform, and add cordova debugging flag (-d).

--- gen-cordova-project.sh
+++ gen-cordova-project.sh
@@ -2,6 +2,8 @@
 
 git clean -d -f
 
-rm -rf /home/nix/.cordova/lib/tizen
+# rm -rf /home/nix/.cordova/lib/tizen
 
-node_modules/cordova/bin/cordova create hello com.example.hello "Hello World"
+node_modules/cordova/bin/cordova -d create hello com.example.hello "Hello World"
+cd hello
+../node_modules/cordova/bin/cordova -d platform add tizen
