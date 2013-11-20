1bd21bab63d563ed6d01ba32253555ab60cefa9b
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-11-20 14:32:05 +0200

    launch-app.sh: Remove user hardcoding.

--- launch-app.sh
+++ launch-app.sh
@@ -4,7 +4,7 @@ app_id="$1"
 host_ip="$2"
 
 if test "x$app_id" = "x" -o "x$host_ip" = "x"; then
-	echo "Usage:" $( basename $0 ) "<app_id> <host_ip>"
+	echo "Usage:" $( basename $0 ) "<app_id> <user@host_ip>"
 	exit 1
 fi
 
@@ -17,5 +17,4 @@ sleep 1
 echo "Launching app and grabbing port ..."
 inspector_port=$( wrt-launcher -s "$app_id" -d | grep '^port: ' | awk '{ print $2; }' )
 
-# Change the ssh command below to reflect your username
-ssh -t nix@"$host_ip" bash -c "'( set -x; echo "'"'"hostfwd_remove ::10123"'"'" | socat unix:/tmp/qemu.sock -; echo "'"'"hostfwd_add ::10123-:$inspector_port"'"'" | socat unix:/tmp/qemu.sock - )'"
+ssh -t "$host_ip" bash -c "'( set -x; echo "'"'"hostfwd_remove ::10123"'"'" | socat unix:/tmp/qemu.sock -; echo "'"'"hostfwd_add ::10123-:$inspector_port"'"'" | socat unix:/tmp/qemu.sock - )'"
